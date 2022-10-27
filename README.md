09755d6215b648c583a49d77260e711240d56012
Robinson Rosales <developerobinsonrosales@gmail.com>
2022-10-26 19:48:11 -0500

    Fix the basic structure

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 Repositorie to practice with go
 
 To execute the code uses
-```sh
+```go
 go run hellow.world.go
+```
+To build a executable file run
+```go
+go build src/main.go
+```
+To execute the executable the new file run
+```sh
+./main
 ```
---
+++ src/main.go
@@ -0,0 +1,17 @@
+/**
+*Folder name where the file is saved
+*The exception is main.go that is the root file*
+**/
+package main
+
+/*Packege import*/
+import (
+	"fmt"
+)
+
+/*Main function*/
+func main() {
+	//Print the message in the console
+	fmt.Println("Hello World")
+
+}
